bddfd7a2f46f249599a2d35c00f5ab30af1c8549
dhcrain <dhcrain@gmail.com>
2016-05-17 14:22:35 -0400

    Optional, tell if guess is close

--- guessing-game.py
+++ guessing-game.py
@@ -1,4 +1,5 @@
 import random
+
 rand_num = random.randint(1, 100)
 turns = 5
 print(rand_num)
@@ -13,8 +14,8 @@ for turn in range(turns):
     elif guess < rand_num:
         print("Too Low, you have {} guesses left.".format(turns))
     else:
-        print("You Win!, {} was the secret number".format(rand_num))
+        print("You Win!, {} was the secret number!".format(rand_num))
         break
 
-    if (guess - rand_num) <= 10:
-        print("You are with in 10 of the Secret Number!")
+    if abs((guess - rand_num)) <= 10:
+        print("You are within 10 of the Secret Number!")
