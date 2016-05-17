66b0624194905d992c14de453d69b02e163d94b6
dhcrain <dhcrain@gmail.com>
2016-05-17 15:02:37 -0400

    Computer guesser is finctional, but not smart enough yet

--- computer-guess.py
+++ computer-guess.py
@@ -3,7 +3,7 @@ import random
 c_guess = random.randint(1, 100)
 turns = 5
 
-print("Trick the computer number Game!\nThe computer has {} guesses to "
+print("Trick Skynet Number Game!\nSkynet has {} guesses to "
       "guess your number between 1 and 100.".format(turns))
 num = input("Give a number between 1 and 100: ")
 
@@ -12,16 +12,17 @@ for turn in range(turns):
         num = int(num)
     except ValueError:
         print("You must pick and number!")
+        break
     else:
         turns -= 1
         if c_guess > num:
-            print("The Computer's guess was {}".format(c_guess))
+            print("high Skynet's guess was {}".format(c_guess))
             c_guess = random.randint(1, c_guess)
         elif c_guess < num:
-            print("The Computer's guess was {}".format(c_guess))
+            print("low Skynet's guess was {}".format(c_guess))
             c_guess = random.randint(c_guess, 100)
         else:
-            print("You Loose!, The computer guessed your secret number, {}!".format(num))
+            print("You Loose!, Skynet's guessed your secret number, {}!".format(num))
             break
 
         if abs((c_guess - num)) <= 10:
