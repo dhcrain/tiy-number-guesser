e9b068f1e5c59d0f9a7aae11b81b98f1a66766dd
dhcrain <dhcrain@gmail.com>
2016-05-17 20:40:40 -0400

    Added a You Loose statmet and reveled the secret number

--- guessing-game.py
+++ guessing-game.py
@@ -5,7 +5,7 @@ turns = 5
 
 print("Guess the number Game!\nYou have {} guesses to "
       "guess the random number between 1 and 100.".format(turns))
-
+print(rand_num)
 for turn in range(turns):
     guess = input("What is your guess? ")
     turns -= 1
@@ -20,6 +20,10 @@ for turn in range(turns):
             print("Too Low, you have {} guesses left.".format(turns))
         else:
             print("You Win!, {} was the secret number!".format(rand_num))
+            break
+
+        if turns == 0:
+            print("You Loose!, {} was the secret number!".format(rand_num))
             break
 
         if abs((guess - rand_num)) <= 10:
